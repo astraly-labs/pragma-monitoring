468eb7f413c15be1f5cd83511cc87f3ebde2fc17
0xevolve <Artevolve@yahoo.com>
2023-12-15 17:57:23 +0100

    feat: update coingecko mapping

--- src/constants.rs
+++ src/constants.rs
@@ -4,4 +4,10 @@ pub(crate) static COINGECKO_IDS: phf::Map<&'static str, &'static str> = phf_map!
     "BTC/USD" => "bitcoin",
     "ETH/USD" => "ethereum",
     "LUSD/USD" => "liquity-usd",
+    "WBTC/USD" => "wrapped-bitcoin",
+    "DAI/USD" => "dai",
+    "USDC/USD" => "usd-coin",
+    "USDT/USD" => "tether",
+    "WSTETH/USD" => "wrapped-steth",
+    "LORDS/USD" => "lords",
 };
