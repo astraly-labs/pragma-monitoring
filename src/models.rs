1990c702d50b6cba4e3ed2a498156d35edba4351
Jordy Romuald <87231934+JordyRo1@users.noreply.github.com>
2024-08-13 15:29:52 +0200

    Feat(optimistic oracle): Table definition (#39)
    * feat(diesel_table): oo_requests table definition
    
    * feat(oo): model definition
    
    * fix(oo): `OO_requests` pub oo members
    
    * feat(oo-schemas): add settled bool
    
    * fix(oo): ids Bigdecimal -> String
    
    * fix(oo-schemas): parameters as nullable
    
    * fix(oo-schemas): expiration as bigInt
    
    * fix(oo-schemas): bond as String
    
    * fix(oo-schemas): expiratioon timestamp as BigDecimal
    
    * fix(oo-schemas): final modifications

--- src/models.rs
+++ src/models.rs
@@ -89,3 +89,32 @@ pub struct VrfRequest {
     pub _cursor: (Bound<i64>, Bound<i64>),
     pub data_id: String,
 }
+
+
+#[derive(Queryable, Debug, QueryableByName, Selectable)]
+#[diesel(primary_key(data_id))]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(table_name = crate::schema::oo_requests)]
+pub struct OORequest {
+    pub network: String, 
+    pub data_id: String, 
+    pub assertion_id: String, 
+    pub domain_id: String, 
+    pub claim: String, 
+    pub asserter: String, 
+    pub disputer: Option<String>, 
+    pub disputed: Option<bool>, 
+    pub callback_recipient: String, 
+    pub escalation_manager: String, 
+    pub caller: String, 
+    pub expiration_timestamp: NaiveDateTime, 
+    pub settled: Option<bool>,
+    pub settlement_resolution: Option<bool>, 
+    pub settle_caller: Option<String>, 
+    pub currency: String, 
+    pub bond: BigDecimal, 
+    pub _cursor:  (Bound<i64>, Bound<i64>),
+    pub identifier: String, 
+    pub updated_at: NaiveDateTime,
+    pub updated_at_tx: String,
+}
