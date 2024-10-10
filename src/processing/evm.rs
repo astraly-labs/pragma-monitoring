3feef0302c90b0f6b95bfc18119fb638f78489a6
azurwastaken <caseaux.maxime@gmail.com>
2024-10-10 19:24:30 +0200

    quick refacto

--- src/processing/evm.rs
+++ src/processing/evm.rs
@@ -14,7 +14,35 @@ use crate::config::Config;
 use crate::constants::EVM_TIME_SINCE_LAST_FEED_UPDATE;
 use crate::{config::get_config, error::MonitoringError};
 
-pub async fn get_all_feed_ids(config: &Guard<Arc<Config>>, client : &Arc<JsonRpcClient<HttpTransport>>) -> Vec<Felt> {
+#[derive(Clone)]
+struct FeedId{
+    pub feed_id : Felt,
+    pub main_type: u64,
+    pub variant: u64,
+}
+
+impl FeedId{
+    pub fn new(feed_id: Felt) -> Self {
+        Self { feed_id: feed_id, main_type: 9999, variant: 999 }
+    }
+
+    pub fn parse_feed_id(&mut self) -> Result<(), MonitoringError> {
+        self.main_type = ((self.feed_id.to_bigint() & BigInt::from(65280)) / BigInt::from(256))
+            .to_u64()
+            .ok_or(MonitoringError::Evm("invalid main type".to_string()))?;
+        self.variant = (self.feed_id.to_bigint() & BigInt::from(255))
+            .to_u64()
+            .ok_or(MonitoringError::Evm(format!("invalid variant for main type {}", self.main_type)))?;
+        Ok(())
+    }
+
+    pub fn to_calldata(&self) -> Result<FixedBytes<32>,MonitoringError>{
+        alloy::sol_types::private::FixedBytes::from_hex(self.feed_id.to_hex_string())
+                    .map_err(|e| MonitoringError::Evm(e.to_string()))
+    }
+}
+
+pub async fn get_all_feed_ids(config: &Guard<Arc<Config>>, client : &Arc<JsonRpcClient<HttpTransport>>) -> Result<Vec<FeedId>, MonitoringError> {
     let feed_registry_address = config
         .feed_registry_address()
         .ok_or(MonitoringError::Evm("Failed to parse feed registry address".to_string()))?;
@@ -30,7 +58,20 @@ pub async fn get_all_feed_ids(config: &Guard<Arc<Config>>, client : &Arc<JsonRpc
         .await
         .map_err(|e| MonitoringError::Evm(e.to_string()))?;
     feed_list.remove(0);
-    feed_list
+
+    let mut feeds: Vec<FeedId> = feed_list.iter().map(|&feed| {FeedId::new(feed)}).collect();
+    for feed in feeds.iter_mut() {
+        feed.parse_feed_id()?;
+    }
+
+    Ok(feeds)
+}
+
+
+
+pub fn monitor_evm_chain() -> Result<(), MonitoringError>{
+
+    Ok(())
 }
 
 pub async fn check_feed_update_state() -> Result<(), MonitoringError> {
@@ -38,22 +79,14 @@ pub async fn check_feed_update_state() -> Result<(), MonitoringError> {
     let evm_config = config.evm_configs();
     
     let client = &config.network().provider;
-    let feed_list = get_all_feed_ids(&config, client).await;
+    let feed_list = get_all_feed_ids(&config, client).await?;
 
     for chain in evm_config.iter() {
         let fl = feed_list.clone();
         for feed in fl.into_iter() {
-            let main_type = ((feed.to_bigint() & BigInt::from(65280)) / BigInt::from(256))
-                .to_u64()
-                .expect("invalid main type");
-            let variant = (feed.to_bigint() & BigInt::from(255))
-                .to_u64()
-                .expect("invalid variant");
-            let feed_id_as_calldata: FixedBytes<32> =
-                alloy::sol_types::private::FixedBytes::from_hex(feed.to_hex_string())
-                    .expect("failed to parse felt id");
-            match main_type {
-                0 => match variant {
+            let feed_id_as_calldata=  feed.to_calldata()?;
+            match feed.main_type {
+                0 => match feed.variant {
                     0 => {
                         let result = chain
                             .pragma
