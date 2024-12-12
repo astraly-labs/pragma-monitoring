5f7c14e740e123c5c8b76b1a91e8b2494cfd627f
Yasser Tahiri <yasserth19@gmail.com>
2024-12-12 22:24:44 +0100

    :recycle: rename cargo-lint job to cargo-bench and adjust CoinGecko API pagination

--- src/coingecko.rs
+++ src/coingecko.rs
@@ -28,7 +28,7 @@ pub async fn get_coingecko_mappings() -> Result<HashMap<String, String>, CoinGec
 
     loop {
         let url = format!(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page={}",
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page={}",
             page
         );
 
